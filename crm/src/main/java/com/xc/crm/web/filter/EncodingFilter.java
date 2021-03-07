43e1e4a25c5e27ad5ddeeae0ea1938fffa8205b3
xuchang <mrxu_mail@sina.com>
2021-03-07 14:57:11 +0800

    1.0

--- crm/src/main/java/com/xc/crm/web/filter/EncodingFilter.java
+++ crm/src/main/java/com/xc/crm/web/filter/EncodingFilter.java
@@ -2,13 +2,8 @@ package com.xc.crm.web.filter;
 
 import javax.servlet.*;
 import java.io.IOException;
-
 public class EncodingFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -20,8 +15,5 @@ public class EncodingFilter implements Filter {
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    @Override
-    public void destroy() {
 
-    }
 }
